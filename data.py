1ea09a5ae4e6b5d92a88a6eb854d1bca625a8367
iniinu969 <127026964+iniinu969@users.noreply.github.com>
2023-03-15 20:05:47 +0700

    Add files via upload

---
+++ data.py
@@ -0,0 +1,38 @@
+isi = {
+    "Kopi_aren": { 
+        "bahan": 
+            {
+            "kopi"  : 30,
+            "gula_merah"  : 20,
+            "air"   : 80,
+            },
+            "harga": 20000.0,
+        },      
+    
+    "Americano":{
+        "bahan": 
+            {
+            "kopi"     :30,
+            "air"   :80,
+            },
+            "harga" : 10000.0,
+        },
+    
+    "latte": { 
+        "bahan": 
+            {
+            "kopi"  :30,
+            "susu"  : 20,
+            "air"   :80,
+            },
+            "harga" : 15000.0,
+        }
+}
+
+uang = 0
+resource = {
+    "kopi"  :180,
+    "air"   :300,
+    "susu"  :150,
+    "gula_merah"    :100
+}
