3fef28d93b44cad6aba83ad17184cae1160774a2
bananasur <127819637+bananasur@users.noreply.github.com>
2023-03-17 13:51:28 +0700

    Add files via upload
    menambah fungsi:
    1. kurangin resource

---
+++ Fungsi.py
@@ -0,0 +1,30 @@
+
+def resource_full(masukan):
+    for item in masukan:
+        if masukan[item] >= resource[item]:
+            print(f"Maaf bahan tidak cukup untuk membuat pesanan Anda {item}")
+            return True
+    return False
+
+
+def input_uang():
+    total_uang = int(input("Masukan uang: Rp."))
+    return total_uang
+
+
+def transaksi_berhasil(masukan_uang, harga_minuman):
+    if masukan_uang >= harga_minuman:
+        kembalian = masukan_uang - harga_minuman
+        print(f"Transaksi berhasil, kembalian Anda {kembalian}")
+        return True
+    else:
+        print("Maaf uang Anda tidak cukup")
+        return False
+
+
+def kurangin_resource(nama_kopi, bahan):
+    for item in bahan:
+        resource[item] -= bahan[item]
+    print(f"Silakan nikmati {nama_kopi}")
+
+
